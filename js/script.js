9afd45c949b83143ee8d9d74ec811b7c3b436eb2
gabrielgsv <gabriel.soaresv@orgsystem.com.br>
2021-02-12 10:10:32 -0300

    improve textures and clouds

--- js/script.js
+++ js/script.js
@@ -20,14 +20,40 @@ camera.position.set(10, 0, 5);
 // camera.lookAt(0, 0, 0);
 controls.update();
 
+const light = new THREE.AmbientLight(0x404040, 2); // soft white light
+scene.add(light);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 2);
+dirLight.position.set(-1, 0, 1).normalize();
+scene.add(dirLight);
+
 //criar esfera com testura de planeta
 const geometry = new THREE.SphereGeometry(5, 32, 32);
-const loader = new THREE.TextureLoader();
-const material = new THREE.MeshBasicMaterial({
-  map: loader.load("../textures/earth.jpg"),
+const textureLoader = new THREE.TextureLoader();
+const material = new THREE.MeshPhongMaterial({
+  specular: 0x333333,
+  shininess: 15,
+  map: textureLoader.load("../textures/earth_atmos.jpg"),
+  specularMap: textureLoader.load("../textures/earth_specular.jpg"),
+  normalMap: textureLoader.load("../textures/earth_normal.jpg"),
+
+  // y scale is negated to compensate for normal map handedness.
+  normalScale: new THREE.Vector2(0.85, -0.85),
 });
 const earth = new THREE.Mesh(geometry, material);
 scene.add(earth);
+
+// clouds
+
+const materialClouds = new THREE.MeshLambertMaterial({
+  map: textureLoader.load("textures/earth_clouds.png"),
+  transparent: true,
+});
+
+const meshClouds = new THREE.Mesh(geometry, materialClouds);
+meshClouds.scale.set(1.005, 1.005, 1.005);
+meshClouds.rotation.z = 0.41;
+scene.add(meshClouds);
 
 //criar animação por frame
 function animate() {
@@ -36,6 +62,7 @@ function animate() {
   controls.update();
 
   earth.rotation.y += 0.004;
+  meshClouds.rotation.y += 0.004;
 
   renderer.render(scene, camera);
 }
